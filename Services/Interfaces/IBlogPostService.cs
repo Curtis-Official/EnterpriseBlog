93659659b19be0b10de1a3802894dd5db52e7172
Curtis Hansen <curtishansenofficial@gmail.com>
2025-07-10 23:01:57 -0700

    Create IBlogPostService Create SharedProject Create ResponseEnvelope

---
+++ Services/Interfaces/IBlogPostService.cs
@@ -0,0 +1,14 @@
+﻿using EnterpriseBlog.Shared;
+
+namespace EnterpriseBlog.Services.Interfaces
+{
+    public interface IBlogPostService
+    {
+        Task<ResponseEnvelope<BlogPostDTO>> CreateBlogAsync(BlogPostDTO blog);
+        Task<ResponseEnvelope<BlogPostDTO>> GetBlogAsync(Guid blogId);
+        Task<ResponseEnvelope<BlogPostDTO>> UpdateBlogAsync(Guid blogId, BlogPostDTO blog);
+        Task<ResponseEnvelope<BlogPostDTO>> DeleteBlogAsync(Guid blogId);
+        Task<ResponseEnvelope<BlogPostDTO>> PublishBlogAsync(Guid blogId);
+        Task<ResponseEnvelope<BlogPostDTO>> UnPublishBlogAsync(Guid blogId);
+    }
+}

b9a9423f18b0efbfcd2dfcec282bf9eac39b67de
Curtis Hansen <curtishansenofficial@gmail.com>
2025-07-11 00:48:39 -0700

    Create: Configure Dev Fallback

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Blazored.LocalStorage;
 using EnterpriseBlog;
 using EnterpriseBlog.DependencyInjection;
+using EnterpriseBlog.Shared;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -15,5 +16,10 @@ builder.Services.RegisterObjectsByLifeTime(new[] { typeof(BlogClientAssemblyMark
 //3rd party Libraries
 builder.Services.AddBlazoredLocalStorage();
 
+//Options
+builder.Services.Configure<DevOptions>(options =>
+{
+    options.IsDevFallbackEnabled = builder.HostEnvironment.IsDevelopment();
+});
 
 await builder.Build().RunAsync();

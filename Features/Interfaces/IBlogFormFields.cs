c90871735d6b79ac1957b32386a0f345cba3658e
Curtis Hansen <curtishansenofficial@gmail.com>
2025-07-28 09:56:11 -0700

    Create: BlogForm

---
+++ Features/Interfaces/IBlogFormFields.cs
@@ -0,0 +1,9 @@
+﻿namespace EnterpriseBlog.Features.Interfaces
+{
+    public interface IBlogFormFields
+    {
+        string Title { get; set; }
+        string Subtitle { get; set; }
+        string BlogContent { get; set; }
+    }
+}

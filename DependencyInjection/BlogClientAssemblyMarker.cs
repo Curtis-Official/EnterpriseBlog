6f65f97f5810a080584423db467018a790df7e6c
Curtis Hansen <curtishansenofficial@gmail.com>
2025-08-21 20:46:13 -0700

    fix: comments

--- DependencyInjection/BlogClientAssemblyMarker.cs
+++ DependencyInjection/BlogClientAssemblyMarker.cs
@@ -3,7 +3,6 @@
     internal class BlogClientAssemblyMarker
     {
         // This class is used to mark the assembly for dependency injection.
-        // It does not contain any members or methods.
         // The purpose is to allow the assembly to be scanned for services.
         // This is a common pattern in ASP.NET Core applications.
         // It helps to organize and group related services together.
